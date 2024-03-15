95533c9b40d3542680f91542a439368afb239a5a
Liong <michaelljonathan16@gmail.com>
2024-03-15 10:33:07 +0700

    not include tc from Fatah, needs to be fixed

--- src/main/java/id/visionplus/v2/TestCase/Premium/TC_Premium_User_Watch_Live_TV.java
+++ src/main/java/id/visionplus/v2/TestCase/Premium/TC_Premium_User_Watch_Live_TV.java
@@ -10,7 +10,7 @@ import id.visionplus.v2.Action.Assertion;
 import id.visionplus.v2.Action.Click;
 import id.visionplus.v2.Action.Input;
 import id.visionplus.v2.Action.Scroll;
-import id.visionplus.v2.Action.Tap;
+//import id.visionplus.v2.Action.Tap;
 import id.visionplus.v2.MainFunction.BaseTest;
 import id.visionplus.v2.TestCase.General.TC_OpenApp;
 import id.visionplus.v2.TestCase.General.TC_Settings;
@@ -20,7 +20,7 @@ public class TC_Premium_User_Watch_Live_TV extends BaseTest {
 	Click click = new Click();
 	Assertion assertion = new Assertion();
 	Input input = new Input();
-	Tap tap = new Tap();
+//	Tap tap = new Tap();
 	TC_Settings settings = new TC_Settings();
 //	String testCaseType = System.getProperty("testCaseType");
 	String testCaseType = "PREMIUM_USER";
@@ -47,18 +47,18 @@ public class TC_Premium_User_Watch_Live_TV extends BaseTest {
 		
 	}
 
-	@Test(priority = 2)
-	public void TC_User_see_success_Transactions() throws IOException, InterruptedException, TimeoutException {
-
-		Thread.sleep(2000);
-		click.clickfilterSuccessTransactionHistory();
-		test.pass("Successfully Clicked Success Transactions");
-		Thread.sleep(5000);
-		Scroll scroll = new Scroll(android);
-		By locator = By.xpath(
-				"//android.widget.Button[@text=\"Premium + Sports 30 Days 23 November 2022 | 16:21 Total Payment Rp 35.000 Payment Status SUCCESS\"]");
-		scroll.scrollUntilElementFound(locator);
-		Thread.sleep(2000);
-	}
+//	@Test(priority = 2)
+//	public void TC_User_see_success_Transactions() throws IOException, InterruptedException, TimeoutException {
+//
+//		Thread.sleep(2000);
+//		click.clickfilterSuccessTransactionHistory();
+//		test.pass("Successfully Clicked Success Transactions");
+//		Thread.sleep(5000);
+//		Scroll scroll = new Scroll(android);
+//		By locator = By.xpath(
+//				"//android.widget.Button[@text=\"Premium + Sports 30 Days 23 November 2022 | 16:21 Total Payment Rp 35.000 Payment Status SUCCESS\"]");
+//		scroll.scrollUntilElementFound(locator);
+//		Thread.sleep(2000);
+//	}
 
 }
