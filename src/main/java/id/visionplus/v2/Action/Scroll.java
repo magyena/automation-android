0a4a17a1d88aec10f210dc0ac5f18229beb24832
Liong <michaelljonathan16@gmail.com>
2024-03-18 10:20:34 +0700

    Error handling to stop when not found

--- src/main/java/id/visionplus/v2/Action/Scroll.java
+++ src/main/java/id/visionplus/v2/Action/Scroll.java
@@ -1,8 +1,11 @@
 package id.visionplus.v2.Action;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,24 +32,29 @@ public class Scroll extends AndroidGesture
 		gesture.scrollToText(text);
 	}
 	
-	public void scrollUntilElementFound(By locator){
-		System.out.println("Arrive at scroll until method");
-		int flag=0;
-		System.out.println("Initiate While Loop");
-        while (flag==0) {
-            try {
-                WebElement element = android.findElement(locator);
-                if (element.isDisplayed()) {
-            		System.out.println("Element Found");
-            		flag=1;
-                    break;
-                }
-            } catch (org.openqa.selenium.NoSuchElementException e) {
-        		System.out.println("Element Not Found, initiate scroll down");
-                scrollDown(0.2);
-                scrollUp(0.3);
-            }
-        }
+	public void scrollUntilElementFound(WebDriver android, By locator) {
+	    int flag = 0;
+	    long startTime = System.currentTimeMillis();
+	    long timeoutInMillis = TimeUnit.SECONDS.toMillis(180);
+
+	    while (flag == 0 && System.currentTimeMillis() - startTime <= timeoutInMillis) {
+	        try {
+	            WebElement element = android.findElement(locator);
+	            if (element.isDisplayed()) {
+	                System.out.println("Element Found");
+	                flag = 1;
+	                break;
+	            }
+	        } catch (NoSuchElementException e) {
+	            System.out.println("Element Not Found, initiate scroll down");
+	            scrollDown(0.2);
+	            scrollUp(0.3);
+	        }
+	    }
+
+	    if (flag == 0) {
+	        System.out.println("Element not found within the timeout period.");
+	    }
 	}
 	
 	public void scrollDown(double value) {
