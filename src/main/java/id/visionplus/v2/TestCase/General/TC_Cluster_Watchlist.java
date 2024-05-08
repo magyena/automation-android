65b74eb66903183217d4a03ff693608f9ee001ff
Vplusnew <fatahalim245@gmail.com>
2024-05-08 12:00:41 +0700

    Update TC_Cluster_Watchlist.java

--- src/main/java/id/visionplus/v2/TestCase/General/TC_Cluster_Watchlist.java
+++ src/main/java/id/visionplus/v2/TestCase/General/TC_Cluster_Watchlist.java
@@ -21,95 +21,95 @@ public class TC_Cluster_Watchlist extends BaseTest {
 	Click click = new Click();
 	Assertion assertion = new Assertion();
 	Tap tap = new Tap();
-//	String testCaseType = System.getProperty("testCaseType");
-	String testCaseType = "FREE";
+	String testCaseType = System.getProperty("testCaseType");
+//	String testCaseType = "FREE";
 	Swipe swipe = new Swipe(android);
 
 	@Test(priority = 1)
 	public void TC_User_add_32_VOD_to_Cluster_Watchlist() throws IOException, InterruptedException, TimeoutException {
 		TC_OpenApp open_app = new TC_OpenApp();
 		open_app.Choose_Login_As(testCaseType);
-//		click.clickViewAll();
-//		test.pass("Sucessfully clicked view All");
-//
-//		assertion.assertTxtViewAll();
-//		test.pass("Successfully assert view All");
-//
-//		Scroll scroll = new Scroll(android);
-//
-//		By locator1 = By.xpath(
-//				"//android.widget.TextView[@resource-id=\"com.zte.iptvclient.android.idmnc:id/stripTitle\" and @text=\"Horror/Thriller\"]");
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODSinners();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODRadio();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVOD12Hari();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODPrince();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODPiknik();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODKatanya();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODadaDewa();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODCatatanAkhirSekolah();
-//		click.clickSaveToWatchlist();
-//		click.clickBack();
-//		scroll.scrollUntilElementFound(locator1);
-//		click.clickVODComic();
-//		int flag = 0;
-//
-//		try {
-//			assertion.assertAddtoListWatchlist();
-//			click.clickSaveToWatchlist();
-//		} catch (Exception e) {
-//			System.out.println("Failed to assertion add to list: " + e.getMessage());
-//			try {
-//				assertion.assertRemoveFromListtWatchlist();
-//
-//			} catch (Exception ex) {
-//				System.out.println("to assertion remove from list: " + ex.getMessage());
-//				flag = 1;
-//			}
-//		}
-//		Thread.sleep(10000);
-//
-//		if (flag == 0) {
-//			click.clickBack();
-//			test.pass("Successfully Press Back");
-//		}
-//
-//		click.pressBack();
-//
-//		By locator2 = By.xpath(
-//				"//android.widget.TextView[@resource-id=\"com.zte.iptvclient.android.idmnc:id/stripTitle\" and @text=\"Watchlist\"]");
-//		scroll.scrollUntilElementFound(locator2);
-//
-//		Swipe swipe = new Swipe(android);
-//		Point start1 = new Point(941, 1399);
-//		Point end1 = new Point(185, 1391);
-//		swipe.swipetoLeft(start1, end1);
-//		test.pass("Successfully Swipe ");
-//		swipe.swipetoLeft(start1, end1);
-//		swipe.swipetoLeft(start1, end1);
-//		swipe.swipetoLeft(start1, end1);
+		click.clickViewAll();
+		test.pass("Sucessfully clicked view All");
+
+		assertion.assertTxtViewAll();
+		test.pass("Successfully assert view All");
+
+		Scroll scroll = new Scroll(android);
+
+		By locator1 = By.xpath(
+				"//android.widget.TextView[@resource-id=\"com.zte.iptvclient.android.idmnc:id/stripTitle\" and @text=\"Horror/Thriller\"]");
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODSinners();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODRadio();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVOD12Hari();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODPrince();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODPiknik();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODKatanya();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODadaDewa();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODCatatanAkhirSekolah();
+		click.clickSaveToWatchlist();
+		click.clickBack();
+		scroll.scrollUntilElementFound(locator1);
+		click.clickVODComic();
+		int flag = 0;
+
+		try {
+			assertion.assertAddtoListWatchlist();
+			click.clickSaveToWatchlist();
+		} catch (Exception e) {
+			System.out.println("Failed to assertion add to list: " + e.getMessage());
+			try {
+				assertion.assertRemoveFromListtWatchlist();
+
+			} catch (Exception ex) {
+				System.out.println("to assertion remove from list: " + ex.getMessage());
+				flag = 1;
+			}
+		}
+		Thread.sleep(10000);
+
+		if (flag == 0) {
+			click.clickBack();
+			test.pass("Successfully Press Back");
+		}
+
+		click.pressBack();
+
+		By locator2 = By.xpath(
+				"//android.widget.TextView[@resource-id=\"com.zte.iptvclient.android.idmnc:id/stripTitle\" and @text=\"Watchlist\"]");
+		scroll.scrollUntilElementFound(locator2);
+
+		Swipe swipe = new Swipe(android);
+		Point start1 = new Point(941, 1399);
+		Point end1 = new Point(185, 1391);
+		swipe.swipetoLeft(start1, end1);
+		test.pass("Successfully Swipe ");
+		swipe.swipetoLeft(start1, end1);
+		swipe.swipetoLeft(start1, end1);
+		swipe.swipetoLeft(start1, end1);
 	}
 
 	@Test(priority = 2, dependsOnMethods = "TC_User_add_32_VOD_to_Cluster_Watchlist")
