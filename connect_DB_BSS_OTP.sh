763a426a11a5878fe3a8a757b8519ac50fb81336
vplusqasvr <vplus.appleid@gmail.com>
2024-05-20 16:02:50 +0700

    change script connect DB

--- connect_DB_BSS_OTP.sh
+++ connect_DB_BSS_OTP.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-# Set variables for SSH parameters
 
-# Receive parameter from outside file
+# Set variables for database parameters
 phoneNumber="$1"
-
-jumphostUser="ubuntu"
-jumphostHost="108.137.34.236"
-jumphostPassword="Welcome.21!--"
-privateKey="./p-kp-jumphost-db.pem"
 
 dbUsername="team_qa"
 dbName="otp"
-dbHost="p-bss-cluster.cluster-crghkxvi5das.ap-southeast-3.rds.amazonaws.com"
+dbHost="10.10.20.20"
 dbPort="5432"
 dbPassword="4321lupa"
 SQLQuery="select otp from otps s where recipient like '%$phoneNumber' and verified=false order by created_at desc limit 1;"
 
-chmod 600 "$privateKey"
-
-# Suppress warning message and connect to the jumphost with the private key to get the OTP value
-otpValue=$(/opt/homebrew/Cellar/sshpass/1.06/bin/sshpass -p "$jumphostPassword" ssh -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "$privateKey" "$jumphostUser@$jumphostHost" 2>/dev/null << EOF
-  PGPASSWORD="$dbPassword" psql -U $dbUsername -d $dbName -h $dbHost -p $dbPort -t -c "$SQLQuery" | grep -oP '\b\d+\b'
-EOF
-)
+# Suppress warning message and connect to the PostgreSQL database directly to get the OTP value
+otpValue=$(PGPASSWORD="$dbPassword" /opt/homebrew/Cellar/postgresql@14/14.10/bin/psql -U "$dbUsername" -d "$dbName" -h "$dbHost" -p "$dbPort" -t -c "$SQLQuery" | grep -oE '\b[0-9]+\b')
 
 # Extract the last line from otpValue
 lastLine=$(echo "$otpValue" | tail -n 1)
 
 # Print the OTP value from the last line
 echo "$lastLine" | grep -o '[0-9]\+'
+
