6fc7bf41964b9b723ac1e526ad6e14f50dfd4cee
vplusqasvr <vplus.appleid@gmail.com>
2024-05-20 16:02:50 +0700

    change script connect DB

--- connect_DB_BSS_OTP.sh
+++ connect_DB_BSS_OTP.sh
@@ -11,10 +11,11 @@ dbPassword="4321lupa"
 SQLQuery="select otp from otps s where recipient like '%$phoneNumber' and verified=false order by created_at desc limit 1;"
 
 # Suppress warning message and connect to the PostgreSQL database directly to get the OTP value
-otpValue=$(PGPASSWORD="$dbPassword" /opt/homebrew/Cellar/postgresql@14/14.12/bin/psql -U "$dbUsername" -d "$dbName" -h "$dbHost" -p "$dbPort" -t -c "$SQLQuery" | grep -oE '\b[0-9]+\b')
+otpValue=$(PGPASSWORD="$dbPassword" /opt/homebrew/Cellar/postgresql@14/14.10/bin/psql -U "$dbUsername" -d "$dbName" -h "$dbHost" -p "$dbPort" -t -c "$SQLQuery" | grep -oE '\b[0-9]+\b')
 
 # Extract the last line from otpValue
 lastLine=$(echo "$otpValue" | tail -n 1)
 
 # Print the OTP value from the last line
 echo "$lastLine" | grep -o '[0-9]\+'
+
